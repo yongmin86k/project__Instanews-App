163c2830ac7642311a4fd3ff1886111ec348f920
Yongmin Kim <yongmin86k@gmail.com>
2019-07-25 14:42:29 -0700

    synchronize select html elements with customized elements by JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(function () { 
+    const $base = $('#jsCustomSelect');
+    const $customSelect = $('.selectDefaultBox');
+    const $optWrapper = $('.selectContainer');
     
+    // synchronize html select elements and custome select elements
+    $optWrapper.on('click', '.optionContainer', function(){
+        const txtOption = $(this).text().trim().toLowerCase();
+        $base.children().filter(function(index, value){
+            const txtBase = $(value).text().trim().toLowerCase();
+            if (txtBase.indexOf(txtOption) >= 0){
+                $base.children().eq(index).prop('selected', true);
+                $customSelect.text( $(value).text() );
+            }
+        }); 
+    });
 
  }) // end of document ready
 
